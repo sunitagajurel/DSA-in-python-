8a84b5aa584ba883a8367bdd57d77192b8a56a69
sunitagajurel <sunita.gajurel49@gmail.com>
2022-01-26 15:09:13 -0800

    stack program

---
+++ day1_stack.py
@@ -0,0 +1,38 @@
+#create stack
+def createStack(): 
+    stack = []
+    return stack  
+
+#push into stack
+def pushStack(stack,item): 
+    return stack.append(item)
+
+
+#pop item from stack 
+def popStack(stack):
+    if len(stack) == 0:
+        return "stack is empty"
+    
+    p = stack.pop()
+    return "popped item is {}".format(p)
+
+#peek into stack
+def peekStack(stack):
+    top = stack[len(stack)-1]
+    return ('the top of stack is {}'.format(top))
+
+stack = createStack()
+pushStack(stack,1)
+pushStack(stack,2)
+pushStack(stack,3)
+popStack(stack)
+print(stack)
+peekStack(stack)
+
+
+
+
+
+
+
+
