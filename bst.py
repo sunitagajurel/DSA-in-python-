9e5b52a36706e4e74439c5864d233ca3f49326f8
sunitagajurel <sunita.gajurel49@gmail.com>
2022-02-16 20:51:56 -0800

    delete for root node added

--- bst.py
+++ bst.py
@@ -4,25 +4,27 @@ class tree:
         self.left_child = None
         self.right_child = None
     
-
+    #visits root -->leftnode ---> rightnode
     def pre_order_traversal (self): 
-        print(self.key)
+        print(self.key,end =" ")
         if self.left_child: 
             self.left_child.pre_order_traversal()
         if self.right_child: 
             self.right_child.pre_order_traversal()
-            
+
+    #visits leftnode-->root -->rightnode     
     def in_order_traversal(self):
         if self.left_child: 
             self.left_child.in_order_traversal()
-        print(self.key)
+        print(self.key,end = " ")
         if self.right_child: 
             self.right_child.in_order_traversal()
-
+    
+    #visits rightnode-->root -->leftnode
     def post_order_traversal(self): 
         if self.right_child:
             self.right_child.post_order_traversal()
-        print(self.key)
+        print(self.key,end = " ")
         if self.left_child: 
             self.left_child.post_order_traversal()
 
@@ -64,29 +66,41 @@ class tree:
             else:
                 return "no node found"
                 
-    
-    def delete_node(self,data): 
+    def delete_node(self,data,curr): 
         if self.key is None: 
             print("tree is empty")
             return 
         if data < self.key : 
             if self.left_child:
-                self.left_child = self.left_child.delete_node(data)
+                self.left_child = self.left_child.delete_node(data,curr)
             else: 
                 print("no node found")
         elif data >self.key: 
             if self.right_child: 
-               self.right_child = self.right_child.delete_node(data) 
+               self.right_child = self.right_child.delete_node(data,curr) 
             else: 
                 print("no node found")
         else: 
             if self.left_child is None: 
                 temp = self.right_child
+                if data == curr: 
+                    self.key = temp.key
+                    self.right_child = temp.right_child
+                    self.left_child = temp.left_child
+                    temp = None
+                    return
+
                 self = None
                 return temp
 
             if self.right_child is None: 
                 temp = self.left_child
+                if data == curr: 
+                    self.key = temp.key
+                    self.right_child = temp.right_child
+                    self.left_child = temp.left_child
+                    temp = None
+                    return
                 self = None
                 return temp 
 
@@ -94,73 +108,30 @@ class tree:
             while node.left_child: 
                 node = node.left_child
             self.key = node.key 
-            self.right_child = self.right_child.delete_node(data)
+            self.right_child = self.right_child.delete_node(data,curr)
         return self
 
-
-
-    def delete(self,data): 
-        if self.key is None: 
-            print("node is empty")
-            return 
-        if self.key < data: 
-            if self.left_child: 
-              self.left_child = self.left_child.delete(data)
-            else: 
-                print("no data found")
-        if self.key > data: 
-            if self.right_child:
-                self.right_child = self.right_child.delete(data)
-            else: 
-                print("no data found")
-        else: 
-            if  self.left_child is None: 
-                temp = self.right_child
-                self = None 
-                return temp
-
-            if self.right_child is None: 
-                temp = self.left_child
-                self = None 
-                return temp 
-
-            node = self.right_child
-            while node.left_child:
-                node = node.left_child 
-            self.key = node.key 
-            self.right_child = self.right_child.delete(node.key)
-            return self
-
-
-
-
-        
-
+def count(node): 
+    if node is None: 
+        return 0 
+    return 1+count(node.left_child) +count(node.right_child)      
 
 root = tree(10)
-root.insert_data(5)
-root.insert_data(25)
-root.insert_data(15)
-root.insert_data(6)
-root.insert_data(2)
+list = [3,50,40,5,14]
+for i in list: 
+    root.insert_data(i)
 
-# print(root.key)
-# print(root.left_child.key)
-# print(root.right_child.key)
-# print(root.left_child.left_child.key)
-# print(root.right_child.left_child.key)
-
+print("no of node in tree is: ", count(root))
 print("max value is :", root.find_max())
 print("min value is :", root.find_min())
 print(root.search_data(50))
 
 root.pre_order_traversal()
-root.delete_node(15)
+if count(root) > 1: 
+    root.delete_node(10,root.key)
+else: 
+    print("can't perform delete operation on node")
 print("inorder traversal")
 root.in_order_traversal()
 print("postorder traversal")
 root.post_order_traversal()
-
-
-
-
