c1b9980b812a2c3c3873c5ac3bf485bc69b69289
sunitagajurel <sunita.gajurel49@gmail.com>
2022-02-19 21:40:19 -0800

    heapify operation for max_heap implemented

--- binaryheap.py
+++ binaryheap.py
@@ -5,32 +5,55 @@ class heap:
         self.right_child = None
     
     #inserts making complete binary tree
-    def insert(self,key):
+    def insert(self,key):  #1,2,3,4,5,6,7
         if self.left_child is None: 
             self.left_child = heap(key)
             return 
         
         if self.right_child is None: 
             self.right_child = heap(key)
-        
+            return 
+
+        if self.left_child.left_child  and self.left_child.right_child:
+            print("new layer")
+            self.right_child.insert(key)
+            return 
         else: 
             self.left_child.insert(key)
 
-
-    
-    def heapify_up(self):
-        # if self.key < self.left_child.key:
-        #     temp = self.left_child.key 
-        #     self.left_child.key = self.key 
-        #     self.key = temp  
-
-        # if self.key < self.right_child.key:
-        #     temp = self.right_child.key 
-        #     self.right_child.key = self.key 
-        #     self.key = temp 
-        return 
-
+    def pre_order_traversal (self): 
+        print(self.key,end =" ")
+        if self.left_child: 
+            self.left_child.pre_order_traversal()
+        if self.right_child: 
+            self.right_child.pre_order_traversal()
         
+    def heapify(self):
+        if self.left_child: 
+            if self.left_child.left_child: 
+              self.left_child.heapify()
+
+            if self.key < self.left_child.key:
+                print('-----')
+                print("workd")
+                temp = self.left_child.key 
+                self.left_child.key = self.key 
+                self.key = temp 
+                self.heapify()
+                
+            if self.right_child:
+                if self.key < self.right_child.key:
+                    print("works")
+                    temp = self.right_child.key 
+                    self.right_child.key = self.key 
+                    self.key = temp
+                    self.heapify() 
+                
+
+        if self.right_child: 
+            self.right_child.heapify()
+
+        return self
 
     def heapify_down(self):
         return 
@@ -38,12 +61,13 @@ class heap:
     def delete_node(self,key): 
         return 
 
-root = heap(10)
-root.insert(4)
-root.insert(6)
-root.insert(5)
+root = heap(4)
+list = [10,18,6,2,20]
+for i in list: 
+    root.insert(i) 
 
-print(root.key)
-print(root.left_child.key)
-print(root.right_child.key)
+
+root.pre_order_traversal()
 print(root.left_child.left_child.key)
+root.heapify()
+root.pre_order_traversal()
